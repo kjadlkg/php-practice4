f00b2bb95fe1a0083af97f807bac1aefa8be3d53
kjadlkg <kjadlkg@gmail.com>
2025-03-07 23:51:01 +0900

    init

---
+++ member/join/join.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>회원가입</title>
+</head>
+
+<body>
+    <div>
+        <h1>회원가입</h1>
+    </div>
+</body>
+
+</html>
---
+++ member/login/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인</title>
+</head>
+
+<body>
+    <div>
+        <h1>로그인</h1>
+        <form>
+            <input type="text" placeholder="아이디" />
+            <input type="password" placeholder="비밀번호" />
+            <input type="button" value="로그인" />
+            <input type="checkbox" id="idsave" />
+            <label for="idsave">아이디 저장</label>
+            <a href="../join/join.php">회원가입</a>
+            <a href="../../forgot/index.php">비밀번호 찾기</a>
+        </form>
+
+    </div>
+</body>
+
+</html>
