643b17f065c311d98f3e3280268551d70e7ba203
kjadlkg <kjadlkg@gmail.com>
2025-03-14 21:24:02 +0900

    forgot

---
+++ member/forgot/forgot.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>비밀번호 변경</title>
+</head>
+
+<body>
+    <form>
+        <h1>비밀번호 변경</h1>
+        <form method="post" action="forgot.php">
+            <input type="password" name="pw" placeholder="새 비밀번호" />
+            <input type="pasword" name="pw_check" placeholder="새 비밀번호 확인" />
+            <input type="submit" value="변경" />
+        </form>
+        </div>
+</body>
+
+</html>
---
+++ member/forgot/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>비밀번호 찾기</title>
+</head>
+
+<body>
+    <div>
+        <h1>비밀번호 찾기</h1>
+        <form method="post" action="index.php">
+            <input type="text" name="id" placeholder="아이디" />
+            <input type="email" name="email" placeholder="이메일" />
+            <input type="submit" value="확인" />
+        </form>
+</body>
+
+</html>
