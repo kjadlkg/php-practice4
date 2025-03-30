ba7b18dabef133876eb371df8663e03f12a2861d
kjadlkg <kjadlkg@gmail.com>
2025-03-30 23:55:48 +0900

    mypage/index - paging - count

--- mypage/index.php
+++ mypage/index.php
@@ -9,6 +9,7 @@ if (!isset($_SESSION['id'])) {
 $id = $_SESSION['id'];
 $name = $_SESSION['name'];
 
+// 게시글
 $stmt = $db->prepare("
 SELECT b.*,
 (SELECT COUNT(*) FROM comment WHERE board_id = b.board_id) AS comment_count
@@ -22,6 +23,7 @@ $stmt->execute();
 $board_result = $stmt->get_result();
 $stmt->close();
 
+// 댓글글
 $stmt = $db->prepare("
 SELECT c.*,
 (SELECT board_title FROM board WHERE board_id = c.board_id) AS board_title
@@ -33,6 +35,23 @@ LIMIT 5
 $stmt->bind_param("s", $name);
 $stmt->execute();
 $comment_result = $stmt->get_result();
+$stmt->close();
+
+// 게시글 개수
+$stmt = $db->prepare("SELECT COUNT(*) AS total_post FROM board WHERE board_writer = ?");
+$stmt->bind_param("s", $name);
+$stmt->execute();
+$post_count_result = $stmt->get_result();
+$post_count = $post_count_result->fetch_assoc()['total_post'];
+$stmt->close();
+
+// 댓글 개수
+
+$stmt = $db->prepare("SELECT COUNT(*) AS total_comment FROM comment WHERE comment_writer = ?");
+$stmt->bind_param("s", $name);
+$stmt->execute();
+$comment_count_result = $stmt->get_result();
+$comment_count = $comment_count_result->fetch_assoc()['total_comment'];
 $stmt->close();
 ?>
 
@@ -51,45 +70,45 @@ $stmt->close();
         <a href="info.php">내 정보</a>
     </div>
     <div>
-        <h3>게시글</h3>
+        <h3>게시글(<?= $post_count ?>)</h3>
         <a href="posting.php">전체보기</a>
         <?php if ($board_result->num_rows == 0) { ?>
-            <p>작성한 글이 없습니다</p>
+        <p>작성한 글이 없습니다</p>
         <?php } else { ?>
-            <table>
-                <?php while ($row = $board_result->fetch_assoc()) { ?>
-                    <tr>
-                        <td>
-                            <a href="../board/view.php?id=<?= htmlspecialchars($row['board_id']); ?>">
-                                <?= htmlspecialchars($row['board_title'], ENT_QUOTES, 'UTF-8') ?>
-                                [<?= $row['comment_count'] ?>]
-                            </a>
-                        </td>
-                        <td><?= $row['created_at'] ?></td>
-                    </tr>
-                <?php } ?>
-            </table>
+        <table>
+            <?php while ($row = $board_result->fetch_assoc()) { ?>
+            <tr>
+                <td>
+                    <a href="../board/view.php?id=<?= htmlspecialchars($row['board_id']); ?>">
+                        <?= htmlspecialchars($row['board_title'], ENT_QUOTES, 'UTF-8') ?>
+                        [<?= $row['comment_count'] ?>]
+                    </a>
+                </td>
+                <td><?= $row['created_at'] ?></td>
+            </tr>
+            <?php } ?>
+        </table>
         <?php } ?>
     </div>
     <div>
-        <h3>댓글</h3>
+        <h3>댓글(<?= $comment_count ?>)</h3>
         <a href="comment.php">전체보기</a>
         <?php if ($comment_result->num_rows == 0) { ?>
-            <p>작성한 댓글이 없습니다</p>
+        <p>작성한 댓글이 없습니다</p>
         <?php } else { ?>
-            <table>
-                <?php while ($row = $comment_result->fetch_assoc()) { ?>
-                    <tr>
-                        <td>
-                            <a href="../board/view.php?id=<? htmlspecialchars($row['comment_id']); ?>">
-                                <?= htmlspecialchars($row['comment_content'], ENT_QUOTES, 'UTF-8'); ?>
-                            </a>
-                        </td>
-                        <td><?= $row['created_at'] ?></td>
-                        <td><?= htmlspecialchars($row['board_title'], ENT_QUOTES, 'UTF-8') ?></td>
-                    </tr>
-                <?php } ?>
-            </table>
+        <table>
+            <?php while ($row = $comment_result->fetch_assoc()) { ?>
+            <tr>
+                <td>
+                    <a href="../board/view.php?id=<? htmlspecialchars($row['comment_id']); ?>">
+                        <?= htmlspecialchars($row['comment_content'], ENT_QUOTES, 'UTF-8'); ?>
+                    </a>
+                </td>
+                <td><?= $row['created_at'] ?></td>
+                <td><?= htmlspecialchars($row['board_title'], ENT_QUOTES, 'UTF-8') ?></td>
+            </tr>
+            <?php } ?>
+        </table>
         <?php } ?>
     </div>
 </body>
