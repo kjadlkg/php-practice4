450810a6c88ddc06f9ec7606fb3be2e0b791dd59
kjadlkg <kjadlkg@gmail.com>
2025-04-14 22:19:11 +0900

    kcaptcha 설정 변경

--- kcaptcha-master/KCAPTCHA.php
+++ kcaptcha-master/KCAPTCHA.php
@@ -32,8 +32,8 @@ class KCAPTCHA
         $fontsdir = 'fonts';
 
         # CAPTCHA string length
-        $length = mt_rand(5, 7); # random 5 or 6 or 7
-        //$length = 6;
+        // $length = mt_rand(5, 7); # random 5 or 6 or 7
+        $length = 3;
 
         # CAPTCHA image size (you do not need to change it, this parameters is optimal)
         $width = 160;
@@ -43,16 +43,16 @@ class KCAPTCHA
         $fluctuation_amplitude = 8;
 
         #noise
-        //$white_noise_density=0; // no white noise
-        $white_noise_density = 1 / 6;
-        //$black_noise_density=0; // no black noise
+        $white_noise_density = 0; // no white noise
+        // $white_noise_density = 1 / 6;
+        // $black_noise_density = 0; // no black noise
         $black_noise_density = 1 / 30;
 
         # increase safety by prevention of spaces between symbols
-        $no_spaces = true;
+        $no_spaces = false;
 
         # show credits
-        $show_credits = true; # set to false to remove credits line. Credits adds 12 pixels to image height
+        $show_credits = false; # set to false to remove credits line. Credits adds 12 pixels to image height
         $credits = 'www.captcha.ru'; # if empty, HTTP_HOST will be shown
 
         # CAPTCHA image colors (RGB, 0-255)
