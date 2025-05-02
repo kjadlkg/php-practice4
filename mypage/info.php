5d81285963c3d385939eef35abda094dd18d8986
kjadlkg <kjadlkg@gmail.com>
2025-05-02 23:56:52 +0900

    mypage/info - css

--- mypage/info.php
+++ mypage/info.php
@@ -29,24 +29,94 @@ if (!$user) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>내 정보</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/component.css">
+    <link rel="stylesheet" href="../css/contents.css">
+    <link rel="stylesheet" href="../css/page/login.css">
 </head>
 
 <body>
-    <div>
-        <h1>내 정보</h1>
-        <a href="../member/forgot/index2.php">비밀번호 변경</a>
-        <form method="post" action="change.php">
-            <label for="name">이름</label>
-            <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name); ?>" required />
-            <label for="id">아이디</label>
-            <span id="id" name="id"><?php echo htmlspecialchars($id); ?></span>
-            <label for="pw">비밀번호</label>
-            <input type="password" id="pw" name="pw" placeholder="비밀번호 확인" required />
-            <label for="email">이메일</label>
-            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user["user_email"]); ?>"
-                required />
-            <input type="submit" value="정보 수정" />
-        </form>
+    <div id="top" class="width868 login_wrap">
+        <header class="header bg">
+            <div class="head">
+                <h1 class="logo">
+                    <a href="../../main/index.php">메인페이지</a>
+                </h1>
+            </div>
+        </header>
+        <main id="container">
+            <div class="content info_change">
+                <article>
+                    <h2 class="blind">개인정보 변경</h2>
+                    <section class="pagemenu">
+                        <h2 class="blind">페이지 메뉴</h2>
+                        <div class="page_menu fr clear">
+                            <a href="../member/forgot/index2.php">비밀번호 변경</a>
+                            <a href="">회원 탈퇴</a>
+                        </div>
+                    </section>
+                    <section>
+                        <div class="content_head">
+                            <h3 class="head_title">기본 정보 변경</h3>
+                        </div>
+                        <div class="content_box border nomin">
+                            <div class="con changeform_box">
+                                <form method="post" action="change.php">
+                                    <fieldset>
+                                        <legend class="blind">기본 정보 변경 폼</legend>
+                                        <div class="form_group nick">
+                                            <div class="form_title">닉네임</div>
+                                            <div class="form_text">
+                                                <input type="text" class="int" id="name" name="name"
+                                                    value="<?php echo htmlspecialchars($name) ?>">
+                                            </div>
+                                        </div>
+                                        <div class="form_group id">
+                                            <div class="form_title">아이디</div>
+                                            <div class="form_text">
+                                                <input id="id" class="int bg" name="id"
+                                                    value="<?php echo htmlspecialchars($id) ?>" readonly>
+                                            </div>
+                                        </div>
+                                        <div class="form_group pw">
+                                            <div class="form_title">비밀번호</div>
+                                            <div class="form_text">
+                                                <input type="password" class="int" id="pw" name="pw"
+                                                    placeholder="비밀번호 확인">
+                                            </div>
+                                        </div>
+                                        <div class="form_group email">
+                                            <div class="form_title">이메일</div>
+                                            <div class="form_text">
+                                                <input type="email" class="int" id="email" name="email"
+                                                    value="<?php echo htmlspecialchars($user["user_email"]) ?>">
+                                            </div>
+                                        </div>
+                                        <div class="btn_box clear">
+                                            <div class="fr">
+                                                <button type="submit" class="btn btn_blue">확인</button>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </section>
+                </article>
+            </div>
+        </main>
+        <footer class="footer">
+            <div class="info_policy">
+                <a href="">회사소개</a>
+                <a href="">제휴안내</a>
+                <a href="">광고안내</a>
+                <a href="">이용약관</a>
+                <a href="">개인정보처리방침</a>
+                <a href="">청소년보호정책</a>
+            </div>
+            <div class="copyright">Copyright ⓒ</div>
+        </footer>
     </div>
 </body>
 
