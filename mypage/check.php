bf72b29fb019100193284381a5968e0ad5c56f8c
kjadlkg <kjadlkg@gmail.com>
2025-05-05 14:03:28 +0900

    mypage/check - css mypage/withdraw - css

--- mypage/check.php
+++ mypage/check.php
@@ -45,32 +45,73 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>비밀번호 확인</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/component.css">
+    <link rel="stylesheet" href="../css/contents.css">
+    <link rel="stylesheet" href="../css/page/login.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-    <div>
-        <p>비밀번호를 입력해주세요</p>
-        <form method="POST">
-            <input type="hidden" id="csrf_token" name="csrf_token" value="<?= get_csrf_token() ?>">
-            <input type="password" id="pw" name="pw" placeholder="비밀번호">
-            <button type="submit">확인</button>
-        </form>
-    </div>
-    <script>
-        $(function () {
-            $('form').on('submit', function (e) {
-                const pw = $('#pw').val().trim();
+    <div id="top" class="width868 login_wrap">
+        <header class="header bg">
+            <div class="head">
+                <h1 class="logo">
+                    <a href="../main/index.php">메인페이지</a>
+                </h1>
+            </div>
+        </header>
+        <main id="container">
+            <div class="content info_change">
+                <article>
+                    <h2 class="blind">개인 정보 변경</h2>
+                </article>
+                <section>
+                    <div class="content_head">
+                        <h3 class="head_title">비밀번호 확인</h3>
+                    </div>
+                    <form method="POST">
+                        <input type="hidden" id="csrf_token" name="csrf_token" value="<?= get_csrf_token() ?>">
+                        <div id="contentbox" class="content_box border">
+                            <div class="con center">
+                                <strong class="title">비밀번호를 입력해주세요</strong>
+                                <div class="form_box">
+                                    <input type="password" class="int" id="pw" name="pw" placeholder="비밀번호">
+                                    <button type="submit" class="btn btn_blue small btn_wfull">확인</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </main>
+        <script>
+            $(function () {
+                $('form').on('submit', function (e) {
+                    const pw = $('#pw').val().trim();
 
-                if (pw === '') {
-                    alert("비밀번호를 입력해주세요.");
-                    e.preventDefault();
-                    $('#pw').focus();
-                    return;
-                }
-            });
-        })
-    </script>
+                    if (pw === '') {
+                        alert("비밀번호를 입력해주세요.");
+                        e.preventDefault();
+                        $('#pw').focus();
+                        return;
+                    }
+                });
+            })
+        </script>
+        <footer class="footer">
+            <div class="info_policy">
+                <a href="">회사소개</a>
+                <a href="">제휴안내</a>
+                <a href="">광고안내</a>
+                <a href="">이용약관</a>
+                <a href="">개인정보처리방침</a>
+                <a href="">청소년보호정책</a>
+            </div>
+            <div class="copyright">Copyright ⓒ</div>
+        </footer>
+    </div>
 </body>
 
 </html>
--- mypage/withdraw.php
+++ mypage/withdraw.php
@@ -11,6 +11,11 @@ $id = $_SESSION['id'];
 $name = $_SESSION['name'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['term_agree'])) {
+        echo "<script>alert('탈퇴 동의에 체크 해주시길 바랍니다.'); history.back();</script>";
+        exit;
+    }
+
     $is_deleted = 1;
     $deleted_at = date("Y-m-d H:i:s");
 
@@ -22,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     session_unset();
     session_destroy();
 
-    echo "<script>alert('탈퇴가 완료되었습니다.); location.href='../main/index.php';</script>";
+    echo "<script>alert('탈퇴가 완료되었습니다.'); location.href='../main/index.php';</script>";
     exit;
 }
 
@@ -34,29 +39,107 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>회원 탈퇴</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/component.css">
+    <link rel="stylesheet" href="../css/contents.css">
+    <link rel="stylesheet" href="../css/page/login.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-    <div>
-        <form method="POST">
-            <p>탈퇴 시 계정 복구가 불가능합니다.</p>
-            <p>탈퇴 시 해당 계정으로 작성된 게시물과 댓글은 자동으로 삭제되지 않습니다.</p>
-            <p>탈퇴 하시겠습니까?</p>
-            <div>
-                <div>
-                    <div>아이디</div>
-                    <p><?= htmlspecialchars($id) ?></p>
-                </div>
-                <div>
-                    <div>닉네임</div>
-                    <p><?= htmlspecialchars($name) ?></p>
-                </div>
+    <div id="top" class="width868 login_wrap">
+        <header class="header bg">
+            <div class="head">
+                <h1 class="logo">
+                    <a href="../main/index.php">메인페이지</a>
+                </h1>
             </div>
-            <div>
-                <button type="submit">탈퇴</button>
-                <button type="button" onclick="location.href='../main/index.php'">취소</button>
+        </header>
+        <main id="container">
+            <div class="content withdraw">
+                <article>
+                    <h2 class="blind">회원 탈퇴</h2>
+                    <section>
+                        <div class="content_head">
+                            <h3 class="head_title">회원 탈퇴</h3>
+                        </div>
+                        <div id="contentbox" class="content_box border">
+                            <div class="con innerbox">
+                                <form method="POST">
+                                    <div class="info_text pit">
+                                        <p>
+                                            <em class="tip_deco_dot red big"></em>
+                                            <b class="title font_red">탈퇴 시 계정 복구가 불가능합니다.</b>
+                                        </p>
+                                        <p>
+                                            <em class="tip_deco_dot red big"></em>
+                                            <b class="title font_red">탈퇴 시 해당 계정으로 작성된 게시물과 댓글은 자동으로 삭제되지 않습니다.</b>
+                                        </p>
+                                    </div>
+                                    <div class="info_text">
+                                        <p>
+                                            <em class="tip_deco_dot"></em>
+                                            탈퇴 처리된 아이디는 사용이 불가능합니다.
+                                        </p>
+                                    </div>
+                                    <div class="bg_box">
+                                        <div class="info">
+                                            <div class="form_group id">
+                                                <div class="form_title">아이디</div>
+                                                <div class="form_text">
+                                                    <b><?= htmlspecialchars($id) ?></b>
+                                                    <a href="index.php" target="_blank"
+                                                        class="btn btn_blue smallest btn_mypage">마이페이지 확인</a>
+                                                </div>
+                                            </div>
+                                            <div class="form_group nick">
+                                                <div class="form_title">닉네임</div>
+                                                <div class="form_text">
+                                                    <p><?= htmlspecialchars($name) ?></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="btn_box clear">
+                                        <div class="checkbox">
+                                            <input type="checkbox" id="term_agree" name="term_agree">
+                                            <label for="term_agree">위 내용을 확인했습니다</label>
+                                        </div>
+                                        <div class="fr">
+                                            <button type="submit" class="btn btn_blue">탈퇴</button>
+                                            <button type="button" class="btn btn_grey"
+                                                onclick="location.href='../main/index.php'">취소</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </section>
+                </article>
             </div>
-        </form>
+        </main>
+        <script>
+            $(function () {
+                $('form').on('submit', function (e) {
+                    if (!$('#term_agree').prop('checked')) {
+                        alert("탈퇴 동의에 체크 해주시길 바랍니다.");
+                        e.preventDefault();
+                    }
+                });
+            })
+        </script>
+        <footer class="footer">
+            <div class="info_policy">
+                <a href="">회사소개</a>
+                <a href="">제휴안내</a>
+                <a href="">광고안내</a>
+                <a href="">이용약관</a>
+                <a href="">개인정보처리방침</a>
+                <a href="">청소년보호정책</a>
+            </div>
+            <div class="copyright">Copyright ⓒ</div>
+        </footer>
     </div>
 </body>
 
